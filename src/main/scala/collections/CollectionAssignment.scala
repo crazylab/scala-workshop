503a5a780e0e06a5fb60b9a06dfc020b88065502
Rahul Nandi <rahulna@thoughtworks.com>
2019-12-17 12:39:18 +0530

    Solve collection assignment

--- src/main/scala/collections/CollectionAssignment.scala
+++ src/main/scala/collections/CollectionAssignment.scala
@@ -2,29 +2,30 @@ package collections
 
 object CollectionAssignment {
   //  Do not use for/while loops while solving these problem
+  private val isOdd: Int => Boolean = _ % 2 != 0
 
-  def lengthOfNames(names: List[String]): List[Int] = ???
+  def lengthOfNames(names: List[String]): List[Int] = names.map(name => name.length)
 
-  def getOdds(numbers: List[Int]): List[Int] = ???
+  def getOdds(numbers: List[Int]): List[Int] = numbers.filter(isOdd)
 
-  def getEvens(numbers: List[Int]): List[Int] = ???
+  def getEvens(numbers: List[Int]): List[Int] = numbers.filter(!isOdd(_))
 
-  def getTotal(numbers: Array[Int]): Int = ???
+  def getTotal(numbers: Array[Int]): Int = numbers.reduce(_ + _)
 
-  def findHighest(numbers: Array[Int]): Int = ???
+  def findHighest(numbers: Array[Int]): Int = numbers.reduce((result, n) => if (result > n) result else n)
 
-  def countOdds(numbers: Seq[Int]): Int = ???
+  def countOdds(numbers: Seq[Int]): Int = numbers.count(isOdd)
 
-  def countEvens(numbers: Seq[Int]): Int = ???
+  def countEvens(numbers: Seq[Int]): Int = numbers.count(!isOdd(_))
 
-  def countOfNumbersAbove(numbers: List[Int], threshold: Int): Int = ???
+  def countOfNumbersAbove(numbers: List[Int], threshold: Int): Int = numbers.count(_ > threshold)
 
   //  Do not use any formula to calculate
-  def sumOfN(n: Int): Int = ???
+  def sumOfN(n: Int): Int = (1 to n).sum
 
   //  Try to do it in one line
-  def lengthOfSentence(sentence: List[String]): Int = ???
+  def lengthOfSentence(sentence: List[String]): Int = sentence.foldLeft(0)((total, word) => total + word.length)
 
   //  Try to do it in one line
-  def calculateRunningTotal(numbers: List[Int]): List[Int] = ???
+  def calculateRunningTotal(numbers: List[Int]): List[Int] = numbers.tail.scan(numbers.head)(_ + _)
 }
